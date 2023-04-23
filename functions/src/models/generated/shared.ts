ace894298ccfbbc21424e98fbc99ed3f22a13253
Methodician <methodician@gmail.com>
2023-04-23 16:48:16 -0700

    Found a good way to share models without funky file structures.

---
+++ functions/src/models/generated/shared.ts
@@ -0,0 +1,20 @@
+// todo: just import and export shared types from other files
+export type ExampleResponse = {
+  id: string;
+  object: string;
+  created: number;
+  model: string;
+  choices: [
+    {
+      text: string;
+      index: number;
+      logprobs: null;
+      finish_reason: string;
+    }
+  ];
+  usage: {
+    prompt_tokens: number;
+    completion_tokens: number;
+    total_tokens: number;
+  };
+};
