ea2a4b9244d9e7fd708b275ef14539504af4e56e
Methodician <methodician@gmail.com>
2023-04-23 10:38:19 -0700

    ng added ngfire

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'openai-exploration-9d32c',
+    appId: '1:192519951060:web:686dd16ce88c4c131b6141',
+    storageBucket: 'openai-exploration-9d32c.appspot.com',
+    apiKey: 'AIzaSyDXI317H2Y8kqGjvgzp3p6kQQGJbe9kO4k',
+    authDomain: 'openai-exploration-9d32c.firebaseapp.com',
+    messagingSenderId: '192519951060',
+  },};
