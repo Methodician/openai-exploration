32d9d4bf71d0311da9208866299fdc879cb6a458
Methodician <methodician@gmail.com>
2023-05-21 07:30:11 -0700

    Removing environments from source control

---
+++ src/environments (examples)/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBlMfme9JSX3YuUKcvESaYNR876GgM4C_8',
+    authDomain: 'jacobs-box.firebaseapp.com',
+    databaseURL: 'https://jacobs-box-default-rtdb.firebaseio.com',
+    projectId: 'jacobs-box',
+    storageBucket: 'jacobs-box.appspot.com',
+    messagingSenderId: '661771800383',
+    appId: '1:661771800383:web:be98117768853d40a5cd8a',
+  },
+};
